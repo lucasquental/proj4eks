8c98e5b3ffac56d0c5676ad32fbff5424fa80785
lucasquental <lucas.quental.001@gmail.com>
2022-02-25 06:54:34 -0800

    update v2 eks

--- 03-eks/eks.tf
+++ 03-eks/eks.tf
@@ -1,5 +1,30 @@
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 provider "aws"{
-  region = "sa-east-1"
+    region = "sa-east-1"
+}
+
+terraform {
+  required_version = ">= 0.14.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.3"
+    }
+  }
 }
 
 module "eks" {
--- 99-tests/eks.tf
+++ 99-tests/eks.tf
@@ -1,3 +1,7 @@
+provider "aws"{
+    region = "sa-east-1"
+}
+
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_id
 }
@@ -10,10 +14,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
-}
-
-provider "aws"{
-    region = "sa-east-1"
 }
 
 terraform {
