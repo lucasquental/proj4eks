d146a9d2fb6da9a33e861a9a4b0d245f786edea6
lucasquental <lucas.quental.001@gmail.com>
2022-03-01 21:25:38 -0800

    helm add

--- 99-tests/eks.tf
+++ 99-tests/eks.tf
@@ -16,6 +16,12 @@ provider "aws"{
     region = "sa-east-1"
 }
 
+provider "helm"{
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 terraform {
   required_version = ">= 0.14.4"
 
@@ -42,3 +48,17 @@ module "eks" {
     }
   ]
 }
+
+/*
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+*/
