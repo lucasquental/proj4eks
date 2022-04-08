5988ed6a9ea2d78358686237108b7aece6f0a7a3
lucasquental <lucas.quental.001@gmail.com>
2022-04-08 11:54:32 -0700

    remove 07-helm

--- 07-helm/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "s3-bucket-tfstate-proj42"
-    region         = "sa-east-1"
-    key            = "06-helm/terraform.tfstate"
-    dynamodb_table = "s3_state_lock"
-  }
-}
