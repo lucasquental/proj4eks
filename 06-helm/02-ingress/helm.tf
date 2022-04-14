2edf2e8ec02d1f38ed2bda86c1c131434d832cb2
jpfumagales <jpfumagales@gmail.com>
2022-04-14 09:05:42 -0300

    update valeus

--- 06-helm/02-ingress/helm.tf
+++ 06-helm/02-ingress/helm.tf
@@ -12,22 +12,3 @@ resource "helm_release" "ingress-nginx" {
   dependency_update = true
   create_namespace = true
 }
-
-  values = [
-    "${file("values.yaml")}"
-  ]
-
-  set {
-    name  = "controller.ingressClassResource.name"
-    value = "nginx"
-  }
-
-  set {
-    name  = "controller.ingressClassResource.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "controller.ingressClassByName"
-    value = "true"
-  }
