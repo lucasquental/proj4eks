ca5376c9c5d9e6888150290725d61bf1345b6a78
jpfumagales <jpfumagales@gmail.com>
2022-04-14 09:23:50 -0300

    update valeus

--- 06-helm/02-ingress/helm.tf
+++ 06-helm/02-ingress/helm.tf
@@ -4,10 +4,15 @@ provider "helm" {
   }
 }
 
+data  " helm_repository "  " entrada " {
+  name =  " ingress " 
+  url   =  " https://kubernetes.github.io/ingress-nginx/ " 
+}
+
 resource "helm_release" "ingress-nginx" {
   name       = "ingress-nginx"
   namespace = "nginx-controller"
-  repository = "https://kubernetes.github.io/ingress-nginx/"
+  repository = data.helm_repository.ingress.metadata.0.name
   chart      = "ingress-nginx/ingress-nginx"
   dependency_update = true
   create_namespace = true
