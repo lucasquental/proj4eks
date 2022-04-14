90756a87027c6c851c6852e7598aeb5ded068cb0
jpfumagales <jpfumagales@gmail.com>
2022-04-14 08:56:26 -0300

    update valeus

--- 06-helm/02-ingress/backend.tf
+++ 06-helm/02-ingress/backend.tf
@@ -3,7 +3,7 @@ terraform {
     encrypt        = true
     bucket         = "s3-bucket-tfstate-proj42"
     region         = "us-east-2"
-    key            = "06-helm-defectdojo/terraform.tfstate"
+    key            = "06-helm-ingress/terraform.tfstate"
     dynamodb_table = "s3_state_lock"
   }
 }
---
+++ 06-helm/02-ingress/helm.tf
@@ -0,0 +1,31 @@
+provider "helm" {
+  kubernetes {}
+}
+
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  namespace = "nginx-controller"
+  repository = "https://kubernetes.github.io/ingress-nginx/"
+  chart      = "ingress-nginx/ingress-nginx"
+  dependency_update = true
+  create_namespace = true
+}
+
+  values = [
+    "${file("values.yaml")}"
+  ]
+
+  set {
+    name  = "controller.ingressClassResource.name"
+    value = "nginx"
+  }
+
+  set {
+    name  = "controller.ingressClassResource.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.ingressClassByName"
+    value = "true"
+  }
