d64d8c0b4eb7ad6a393609a92ca5bd325849c4a1
lucasquental <lucas.quental.001@gmail.com>
2022-04-27 10:58:10 -0700

    add type on variables

--- hml/02-security-groups/variables.tf
+++ hml/02-security-groups/variables.tf
@@ -1,21 +1,84 @@
-variable "aws_region" {}
+variable "aws_region" {
+    type = string
+}
 
-variable "vpc_aws" {}
+variable "vpc_aws" {
+    type = string
+}
 
-variable "tag_name_bastion" {}
-variable "tag_name_eks" {}
-variable "tag_name_db" {}
+variable "tag_name_bastion" {
+    type = string
+}
+variable "tag_name_eks" {
+    type = string
+}
+variable "tag_name_db" {
+    type = string
+}
 
-variable "description_bastion" {}
-variable "description_eks" {}
-variable "description_db" {}
+variable "description_bastion" {
+    type = string
+}
+variable "description_eks" {
+    type = string
+}
+variable "description_db" {
+    type = string
+}
 
-variable "cidr_block" {}
+variable "cidr_block" {
+    type = list(string)
+}
 
-variable "ingress_rules_bastion" {}
-variable "egress_rules_bastion" {}
+variable "ingress_rules_bastion" {  
+    type = list(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_block  = string
+    description = string
+  }))
+}
+variable "egress_rules_bastion" {
+    type = list(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_block  = string
+    description = string
+  }))
+}
 
-variable "ingress_rules_eks" {}
+variable "ingress_rules_eks" {
+    type = list(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_block  = string
+    description = string
+  }))
+}
 
-variable "sg_ingress_rules" {}
-variable "sg_egress_rules" {}
+variable "sg_ingress_rules" {
+    type = list(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_block  = string
+    description = string
+  }))
+}
+variable "sg_egress_rules" {
+    type = list(object({
+    type        = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_block  = string
+    description = string
+  }))
+}
